a485407021df9ecee8a2ecfce04472c0c78f4575
Nicholas Ormrod <nicholas.ormrod+github@gmail.com>
2024-12-16 18:10:43 -0800

    work

---
+++ KNOWLEDGE.md
@@ -0,0 +1,5 @@
+# Misc Information
+
+Find factorio images on my Mac:
+`~/Library/Application Support/Steam/steamapps/common/Factorio/.../data/base/graphics/...`
+Use the `open` command to see the pngs
--- serde.py
+++ serde.py
@@ -14,31 +14,3 @@ def serialize(b):
     e = base64.b64encode(c)
     z = b'0' + e
     return z
-
-
-"""
-io_data_dir = 'data'
-
-def read(filename):
-  with open(os.path.join(io_data_dir, filename), 'r') as f:
-    return f.read().strip()
-
-def write(filename, content):
-  with open(os.path.join(io_data_dir, filename), 'w') as f:
-    f.write(content)
-
-def load(name):
-  infile = f"hashed_{name}_in.txt"
-  human_infile = f"{name}_in.txt"
-  result = deserialize(read(infile))
-  pretty = json.dumps(result, indent=2)
-  write(human_infile, pretty)
-  return result
-
-def dump(name, j):
-  outfile = f"hashed_{name}_out.txt"
-  human_outfile = f"{name}_out.txt"
-  pretty = json.dumps(j, indent=2)
-  write(human_outfile, pretty)
-  write(outfile, str(serialize(j))[2:-1] + '\n')
-"""
